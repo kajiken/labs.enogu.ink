b09ad9e444bcb8fe12095ae2c76404c8610dcf5c
kajiken <knskjwr@gmail.com>
2021-06-26 12:00:51 +0900

    add placeholder

--- site/components/AvatarImage.tsx
+++ site/components/AvatarImage.tsx
@@ -8,6 +8,8 @@ export default function GitHubIcon() {
       className={styles.avatar}
       width={"64"}
       height={"64"}
+      blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNkmHGmHgAD6QHldFz/TQAAAABJRU5ErkJggg=="
+      placeholder="blur"
       alt="Kensuke KAJIWARA"
     />
   );
